d1942e29e4fd6b634cbd1063072011b94700bf98
Radian06 <aksen05aksen@gmail.com>
2025-09-11 21:23:58 +0900

    ⚡️ :: 헤더 퍼블리싱

---
+++ src/component/header.jsx
@@ -0,0 +1,22 @@
+import "../component/Css/header.css";
+import mainLogo from "../image/mainLogo.png";
+
+import { Link } from "react-router-dom";
+
+function Header() {
+
+
+    return (
+        <div className="header_page">
+            <img src={mainLogo} alt="main logo" className="header_logo"></img>
+            <div className="header_nav_box">
+                <Link to="/reviewCheck" className="header_nav">리뷰 탐지</Link>
+                <Link to="/record" className="header_nav">기록 조회</Link>
+                <Link to="/statics" className="header_nav">통계 정보</Link>
+                <Link to="/login" className="header_nav">로그인</Link>
+            </div>
+        </div>
+    );
+}
+
+export default Header;
