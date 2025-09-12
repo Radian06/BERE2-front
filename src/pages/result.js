38addf3541d5b652d986ef99619ee99de69a0e37
Radian06 <aksen05aksen@gmail.com>
2025-09-12 18:07:08 +0900

    ⚡️ : 결과페이지 버튼 기능 추가

--- src/pages/result.js
+++ src/pages/result.js
@@ -1,6 +1,8 @@
 import "../component/Css/result.css";
 import ResultProgress from "../component/resultProgress";
 
+import { Link } from "react-router-dom";
+
 function Result() {
     return (
         <div className="result_page">
@@ -14,8 +16,8 @@ function Result() {
                         Officia est id exercitation mollit id ipsum minim et. Veniam irure do eu et eu do velit magna eiusmod commodo minim cillum tempor eiusmod. Quis duis ut dolor quis voluptate.
                     </div>
                     <div className="result_btn_box">
-                        <div className="result_btn">돌아가기</div>
-                        <div className="result_btn">자세히 보기</div>
+                        <Link to="/" className="result_btn">돌아가기</Link>
+                        <Link to="/result-detailed" className="result_btn">자세히 보기</Link>
                     </div>
                 </div>
             </div>
