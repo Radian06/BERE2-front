4f3935abcb2a49cc03972b395f05a585bdda65f3
Radian06 <aksen05aksen@gmail.com>
2025-09-12 15:28:01 +0900

    ⚡️ :리뷰조회 페이지  퍼블리싱

--- src/pages/reviewCheck.js
+++ src/pages/reviewCheck.js
@@ -1,9 +1,15 @@
 import "../component/Css/reviewCheck.css";
+import Logo from "../image/logo.png";
 
 function ReviewCheck() {
     return (
         <div className="RC_page">
-            
+            <img src={Logo} alt="" className="RC_logo"></img>
+            <div className="RC_title">BEstREview</div>
+            <div className="RC_input_box">
+                <input type="text" className="RC_input" placeholder="URL을 입력해 주세요." />
+                <button className="RC_input_btn">조회</button>
+            </div>
         </div>
     );
 }
