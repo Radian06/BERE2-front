f57181030c2e9cccc6bf48c520c109ebfe3cd6f5
Radian06 <aksen05aksen@gmail.com>
2025-09-12 22:29:12 +0900

    📄 :: 로그인 페이지 파일 생성

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Intro from "./pages/intro";
 import ReviewCheck from "./pages/reviewCheck";
 import Result from "./pages/result";
 import Record from "./pages/record";
+import Login from "./pages/login";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/review-check" element={<ReviewCheck />} />
         <Route path="/result" element={<Result />} />
         <Route path="/record" element={<Record />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
