a91d6102b41b549d86e8047dda98a04f46360254
Radian06 <aksen05aksen@gmail.com>
2025-09-13 15:46:53 +0900

    ⚡️  자세히보기  페이지 퍼블리싱

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Intro from "./pages/intro";
 import ReviewCheck from "./pages/reviewCheck";
 import Result from "./pages/result";
+import ResultDetailed from "./pages/resultDetailed";
 import Record from "./pages/record";
 import Statics from "./pages/statics";
 
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<Intro />} />
         <Route path="/review-check" element={<ReviewCheck />} />
         <Route path="/result" element={<Result />} />
+        <Route path="/result-detailed" element={<ResultDetailed />} />
         <Route path="/record" element={<Record />} />
         <Route path="/statics" element={<Statics />} />
       </Routes>
---
+++ src/pages/resultDetailed.js
@@ -0,0 +1,15 @@
+import "../component/Css/resultDetailed.css";
+import Header from "../component/header.jsx";
+
+function ResultDetailed() {
+    return (
+        <>
+            <Header />
+            <div className="RD_page">
+
+            </div>
+        </>
+    );
+}
+
+export default ResultDetailed;
