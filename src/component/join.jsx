ad8b3170f5c2b621fd747c50af74184757644111
Radian06 <aksen05aksen@gmail.com>
2025-09-13 12:44:41 +0900

    ⚡️ : 회원가입  모달  퍼블리싱

--- src/component/join.jsx
+++ src/component/join.jsx
@@ -1,15 +1,23 @@
 import "../component/Css/join.css";
 
 function Join() {
-  return (
-    <div className="join_page">
-      <div className="join_title">회원가입</div>
-      <input type="text" className="join_input" placeholder="아이디" />
-      <input type="password" className="join_input" placeholder="비밀번호" />
-      <input type="password" className="join_input" placeholder="비밀번호 확인" />
-      <button className="join_btn">회원가입</button>
-    </div>
-  );
+    return (
+        <div className="join_page">
+            <div className="join_title">회원가입</div>
+            <input type="text" className="join_input" placeholder="아이디" />
+            <input type="password" className="join_input" placeholder="비밀번호" />
+            <input type="password" className="join_input" placeholder="비밀번호 확인" />
+            <input type="email" className="join_input" placeholder="이메일 (선택)" />
+            <label className="join_checkbox_label">
+                <input type="checkbox" className="join_checkbox" />
+                개인정보약관 동의
+            </label>
+            <div className="join_btn_box">
+                <button className="join_back_btn">가입 취소</button>
+                <button className="join_btn">가입하기</button>
+            </div>
+        </div>
+    );
 }
 
 export default Join;
