49dfb434367206d7c0bfba404da1526b82029306
Radian06 <aksen05aksen@gmail.com>
2025-09-15 12:48:01 +0900

    ⚡️ :: 푸터 퍼블리싱

--- src/component/footer.jsx
+++ src/component/footer.jsx
@@ -3,7 +3,15 @@ import "../component/Css/footer.css";
 function Footer() {
     return (
         <div className="footer_page">
-            
+            <div className="footer_box">
+                <div className="footer_top_box">
+                    <div className="footer_nav">문의하기</div>
+                    <div className="footer_nav">이용약관</div>
+                </div>
+                <div className="footer_bottom_box">
+                    <div className="footer_copyright">ⓒ 2025 SOFTWARE CONVERGENCE GYEONGKUK NATIONAL UNIVERSITY</div>
+                </div>
+            </div>
         </div>
     );
 }
